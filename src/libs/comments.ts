06c73a8aa7ab664dab9b616887282512565aa3f9
PanatKaewlangka <panut4207315@gmail.com>
2024-09-30 15:52:13 +0700

    ok

--- src/libs/comments.ts
+++ src/libs/comments.ts
@@ -3,19 +3,19 @@ export const comments = [
     username: "Lisa",
     userImagePath: "/profileImages/lisa.jpg",
     commentText: "จริงค่าาาาา",
-    likeNum: 555,
+    likeNum: 999,
     replies: [
-      {
-        username: "Cat Meme",
-        userImagePath: "/profileImages/popcat.png",
-        replyText: "ลิซ่าาาาาาา Rockstar",
-        likeNum: 20,
-      },
       {
         username: "หมาน้อย",
         userImagePath: "/profileImages/puppy.jpg",
         replyText: "จริงด้วยค้าบบบบบบบบ",
         likeNum: 0,
+      },
+      {
+        username: "Cat Meme",
+        userImagePath: "/profileImages/popcat.png",
+        replyText: "ลิซ่าาาาาาา",
+        likeNum: 2,
       },
       ,
     ],
@@ -23,7 +23,7 @@ export const comments = [
   {
     username: "Charlie Brown",
     userImagePath: "/profileImages/charliebrown.jpg",
-    commentText: "บ้าไปแล้ว... หรือเปล่า?",
+    commentText: "บ้าไปแล้ว",
     likeNum: 0,
     replies: [],
   },
